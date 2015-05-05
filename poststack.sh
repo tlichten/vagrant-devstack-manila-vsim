f4ff829bee0466e5ad5b54d4b54e81d7b74bd743
Thomas <Thomas.Lichtenstein@netapp.com>
2015-05-05 06:27:59 +0200

    Update poststack.sh

--- poststack.sh
+++ poststack.sh
@@ -16,5 +16,19 @@ cd /opt/stack/horizon && cp openstack_dashboard/local/local_settings.py.example 
 sed -i "s/'js_spec_files': \[\],/'js_spec_files': \[\],\n'customization_module': 'manila_ui.overrides',/" /opt/stack/horizon/openstack_dashboard/local/local_settings.py
 sudo -H pip install -e /opt/stack/manila-ui
 cd /opt/stack/horizon && cp ../manila-ui/manila_ui/enabled/_90_manila_*.py openstack_dashboard/local/enabled
-sudo /etc/init.d/apache2 restart
+sudo /etc/init.d/apache2 restart 2>/dev/null
+
+# If you installed Horizon on this server you should be able
+# to access the site using your browser.
+if is_service_enabled horizon; then
+echo "Horizon is now available at http://$OS_HOST_IP/"
+fi
+# If Keystone is present you can point ``nova`` cli to this server
+if is_service_enabled key; then
+echo "Examples on using novaclient command line is in exercise.sh"
+echo "The default users are: admin and demo"
+echo "The password: $ADMIN_PASSWORD"
+echo "Devstack VM console available running: vagrant ssh devstackvm"
+echo "VSim available running: vagrant ssh vsim"
+fi
 
